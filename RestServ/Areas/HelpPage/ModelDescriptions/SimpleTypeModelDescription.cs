fca6a10790c73e33cd39e0d53b8700719ed23c8c
Rasa Cosmin <cosmin.rasa@freshbyteinc.com>
2015-09-30 23:04:00 +0300

    first commit to person API

---
+++ RestServ/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestServ.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RestServ/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using RestServ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestServ.Controllers
+{
+   
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Person> Get()
+        {
+            var personList = new List<Person>();
+
+            for (int i = 0; i < 5; i++)
+                personList.Add(new Person
+                {
+                    Address = "address " + i,
+                    Email = "email " + i,
+                    FirstName = "firstName "+i,
+                    LastName = "lastName "+i,
+                    Id = "id " +i                    
+                });
+
+            return personList;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
